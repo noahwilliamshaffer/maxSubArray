e23204e648cb25473178dec60d6abe35ffc8e514
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2020-08-08 12:47:52 -0400

    Update maxSubArray.cpp

--- maxSubArray.cpp
+++ maxSubArray.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int maxSubArray(vector<int> nums) {
         int n = nums.size();
+        //Initilize current and max sum to the first element in the array
         int currSum = nums[0], maxSum = nums[0];
         
+        //loop through the array
         for(int i = 1; i < n; ++i){
+            //current sum will be the max sum between two indexes in the array
             currSum =  max(nums[i], currSum + nums[i]);
-            maxSum = max(maxSum,currSum);
+            //maxSum will be be the max of either the current maxsum or the new currentsum
+            maxSum = max(maxSum,currSum); 
         }
         return maxSum;
     }
